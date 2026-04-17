b71d7607162b9283b9cef7aad842f076542efacd
Saksham Srivastava <srivastavasaksham04@gmail.com>
2026-04-17 23:48:38 +0530

    Added base url
    Added base url

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const api = axios.create({ baseURL: 'https://vehiclemanagementsystem-3.onrender.com + /api' });
+const api = axios.create({ baseURL: 'https://vehiclemanagementsystem-3.onrender.com/api' });
 
 
 api.interceptors.request.use(config => {
